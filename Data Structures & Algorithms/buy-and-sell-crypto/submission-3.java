73aeeef9474c0808643a451866395e35b73a5465
Atharv <110805757+bhosaleatharv89@users.noreply.github.com>
2026-04-20 08:30:10 +0530

    Add: buy-and-sell-crypto - submission-3

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-3.java
@@ -0,0 +1,15 @@
+// right to left scan (not practically usefull)
+class Solution {
+    public int maxProfit(int[] prices) {
+        int maxSell = 0;
+        int maxDiff = 0;
+        for(int i = prices.length-1 ;i>=0; i--)
+        {
+            maxSell = Math.max(maxSell,prices[i]);
+            maxDiff = Math.max(maxDiff,maxSell - prices[i]);
+        }
+        if(maxDiff<0)return 0;
+        return maxDiff;
+        
+    }
+}
